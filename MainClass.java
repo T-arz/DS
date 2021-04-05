75da025261eb6035a6ae0a73b488343c1d90d9ca
T-arz <74115952+T-arz@users.noreply.github.com>
2021-04-05 17:54:55 -0300

    Add files via upload

--- MainClass.java
+++ MainClass.java
@@ -1,40 +1,27 @@
-package listani;
-
 import java.util.Scanner;
-import java.util.ArrayList;
 
-class MainClass{
-  
-  	static Scanner read = new Scanner(System.in);
-  	static ArrayList<Animal> animais = new ArrayList();
+public class MainClass {
+	static Scanner read = new Scanner(System.in);
 
-  	public static void main(String[] args) {
-    
-		Animal animal;
+	public static void main(String [] args) {
 		int menu = 0;
+		int anos ;
 		
-		while(menu == 0){
-		animal = new Animal();
-		
-		System.out.println("Insira a espécie do animal:");
-		animal.especie = read.next();
-		System.out.println("Insira a raça do animal:");
-		animal.raça = read.next();
-		System.out.println("Insira o peso do animal:");
-		animal.peso = read.nextFloat();
-		System.out.println("Insira a cor do aniaml:");
-		animal.cor = read.next();
-
-
-		animais.add(animal);
-
-		System.out.println("Deseja continuar? 0 para sim , qualquer outro para não.");
-		menu = read.nextInt();
-		}
-
-		System.out.println("Espécie\tRaça\tPeso\tCor");
-		for(int i = 0; i < animais.size(); i++){
-		System.out.println(animais.get(i).especie +"\t"+ animais.get(i).raça +"\t"+ animais.get(i).peso  +"\t"+ animais.get(i).cor);
+		while(menu == 0) {
+			System.out.println("Para avaliar seu nível por favor insira quantos anos de estudo");
+			anos = read.nextInt();
+			if (anos <= 1) {
+				System.out.println("Seu nivel é iniciante");
+			}else if (anos <= 3) {
+				System.out.println("Seu nivel é intermediário");
+			}else if (anos <= 6) {
+				System.out.println("Seu nivel é avançado");
+			}else {
+				System.out.println("Você é um mestre jedi");
+			}
+			
+			System.out.println("Caso queira continuar digite 0 , caso não qualquer outra coisa");
+			menu = read.nextInt();
 		}
 	}
 }
